5e817bee6079f588ae1ddc78279f3b2c53b99abe
juansimonEAFIT <jsospinam@eafit.edu.co>
2025-07-27 13:31:44 -0500

    Seccion B finalizada

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render # here by default
+from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+# Create your views here.
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is my first Django project.",
+            "author": "Developed by: Juan Simon",
+        })
+        
+        return context
+
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "Contact - Online Store",
+            "subtitle": "Contact Us",
+            "email": "contact@onlinestore.com",
+            "address": "123 Fake Street, Medellín",
+            "phone": "+57 300 123 4567",
+        })
+        return context
