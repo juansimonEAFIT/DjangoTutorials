eedf7759aa5728947aa9533a261bdfedfc7df12d
juansimonEAFIT <jsospinam@eafit.edu.co>
2025-07-27 14:22:32 -0500

    Seccion C terminada

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django import forms
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -33,3 +37,40 @@ class ContactPageView(TemplateView):
             "phone": "+57 300 123 4567",
         })
         return context
+
+class Product:
+    products = [
+        {"id":"1", "name":"TV", "description":"Best TV", "price": 1000},
+        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price": 1200},
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price": 50},
+        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price": 200}
+    ]
+
+
+class ProductIndexView(View):   
+    template_name = 'products/index.html'
+
+    def get(self, request):
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] = "List of products"
+        viewData["products"] = Product.products
+
+        return render(request, self.template_name, viewData)
+
+class ProductShowView(View):
+    template_name = 'products/show.html'
+
+    def get(self, request, id):
+        try:
+            product = Product.products[int(id)-1]
+        except (IndexError, ValueError):
+            return HttpResponseRedirect(reverse('home'))
+
+        viewData = {
+            "title": product["name"] + " - Online Store",
+            "subtitle": product["name"] + " - Product information",
+            "product": product
+        }
+        
+        return render(request, self.template_name, viewData)
